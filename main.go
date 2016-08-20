23d2cbc6e05b7dafc4e7eb59af2c34d922de6be8
Jacob Taylor <jacob@usekahuna.com>
2016-08-19 17:34:39 -0700

    fixed #3, fixed #2 Initial implementation of directory tracking with subfolder support

--- main.go
+++ main.go
@@ -4,13 +4,94 @@ package main
 
 import (
 	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	fmt.Printf("replicat online....")
+	fmt.Printf("replicat online....\n")
 
-	fuseMe()
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
 	defer fmt.Printf("End of line\n")
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("file updated:", event.Name)
+				} else {
+					log.Println("event:", event)
+				}
+
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	fmt.Printf("about to call createListOfFolders\n")
+	listOfFolders, err := createListOfFolders("/tmp/foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Done from createListOfFolders: %v\n", listOfFolders)
+	for _, folder := range listOfFolders {
+		err = watcher.Add(folder)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	<-done
+
 }
 
+func createListOfFolders(basePath string) ([]string, error) {
+	paths := make([]string, 0, 100)
+	pendingPaths := make([]string, 0, 100)
+	pendingPaths = append(pendingPaths, basePath)
+
+	for len(pendingPaths) > 0 {
+		currentPath := pendingPaths[0]
+		paths = append(paths, currentPath)
+		pendingPaths = pendingPaths[1:]
+
+		log.Printf("%3d pending - Processing path: %s", len(pendingPaths), currentPath)
+
+		// Read the directories in the path
+		f, err := os.Open(currentPath)
+		if err != nil {
+			return nil, err
+		}
+		dirEntries, err := f.Readdir(-1)
+		for _, entry := range dirEntries {
+			if entry.IsDir() {
+				entry.Mode()
+				newDirectory := filepath.Join(currentPath, entry.Name())
+				pendingPaths = append(pendingPaths, newDirectory)
+			}
+		}
+		f.Close()
+		if err != nil {
+			return nil, err
+		}
+
+		fmt.Printf("*************************************************************\n")
+		fmt.Printf("SubDirectory List \n")
+		for _, dir := range pendingPaths {
+			fmt.Printf("%s\n", dir)
+		}
+		fmt.Printf("*************************************************************\n")
+	}
+
+	return paths, nil
+}
 
