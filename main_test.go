5e0ae403d2546cda0e9c93f68702848a96d28ef7
James Sprinkle <james@usekahuna.com>
2016-09-08 22:29:32 -0700

    Added tests

--- main_test.go
+++ main_test.go
@@ -46,12 +46,112 @@ func TestDirectoryScan(t *testing.T) {
 
 	//changed, updatedState, newPaths, deletedPaths, matchingPaths := checkForChanges(globalSettings.Directory, emptyState)
 
-	// todo make some changes and verify that it is working correctly.
-	// add paths
-	// a, b, c, d, e, ab,abc,abd
+	// add longer paths
+	subDirs := []string{"a", "b", "c"}
+	baseDir := tmpFolder + "/a0"
+	addSubDirs(t, baseDir, subDirs)
+	totalFolders += len(subDirs)
+
+	changed, updatedState, newPaths, deletedPaths, matchingPaths = checkForChanges(tmpFolder, dirState)
+	if !changed || (len(newPaths) + len(deletedPaths) + len(matchingPaths) != totalFolders) {
+		t.Fatal("comparision of current state with current state did not result in empty....ouch\n")
+		t.Fail()
+	}
+	if len(newPaths) != len(subDirs) {
+		t.Fatal(fmt.Sprintf("wrong number of new paths. expected %d, got %d....ouch\n", len(subDirs), len(newPaths)))
+		t.Fail()
+	}
+
+	// get caught up and add more!
+	dirState, err = createListOfFolders(tmpFolder)
+
+	subDirs = []string{"1", "2", "3", "4", "5"}
+	baseDir = tmpFolder + "/a1"
+	addSubDirs(t, baseDir, subDirs)
+	totalFolders += len(subDirs)
+
+	changed, updatedState, newPaths, deletedPaths, matchingPaths = checkForChanges(tmpFolder, dirState)
+	if !changed || (len(newPaths) + len(deletedPaths) + len(matchingPaths) != totalFolders) {
+		t.Fatal("comparision of changed state with current state does not add up....ouch\n")
+		t.Fail()
+	}
+	if len(newPaths) != len(subDirs) {
+		t.Fatal(fmt.Sprintf("wrong number of new paths. expected %d, got %d....ouch\n", len(subDirs), len(newPaths)))
+		t.Fail()
+	}
+
+	// add a, b, c, d, e, ab, abc, abd
+	tmpFolder, err = ioutil.TempDir("", "blank")
+	totalFolders = 1
+	defer os.RemoveAll(tmpFolder)
+	subDirs = []string{"a", "b", "c", "d", "e", "ab", "abc", "abd"}
+	for i:= range subDirs {
+		path := fmt.Sprintf("%s/%s", tmpFolder, subDirs[i])
+		err := os.Mkdir(path, os.ModeDir + os.ModePerm)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	totalFolders += len(subDirs)
+	dirState, err = createListOfFolders(tmpFolder)
+
+	changed, updatedState, newPaths, deletedPaths, matchingPaths = checkForChanges(tmpFolder, dirState)
+	if changed || (len(newPaths) + len(deletedPaths) + len(matchingPaths) != totalFolders) {
+		t.Fatal("comparision of current state with current state did not result in empty....ouch\n")
+		t.Fail()
+	}
+
 	// delete ab and make sure it is the only one deleted
-	// delete the start and end ones
-	// recreate them
+	deletePath := tmpFolder + "/ab"
+	os.Remove(deletePath)
+	changed, updatedState, newPaths, deletedPaths, matchingPaths = checkForChanges(tmpFolder, dirState)
+	if !changed || (len(newPaths) + len(deletedPaths) + len(matchingPaths) != totalFolders) {
+		t.Fatal("comparision of changed state with current state does not add up....ouch\n")
+		t.Fail()
+	}
+	if len(deletedPaths) != 1 {
+		t.Fatal(fmt.Sprintf("wrong number of deleted paths. expected 1, got %d....ouch\n", len(deletedPaths)))
+		t.Fail()
+	}
+
+	// get caught up and delete the start and end ones
+	totalFolders -= len(deletedPaths)
+	dirState, err = createListOfFolders(tmpFolder)
+	deletePath = tmpFolder + "/a"
+	os.Remove(deletePath)
+	deletePath = tmpFolder + "/abd"
+	os.Remove(deletePath)
+	changed, updatedState, newPaths, deletedPaths, matchingPaths = checkForChanges(tmpFolder, dirState)
+	if !changed || (len(newPaths) + len(deletedPaths) + len(matchingPaths) != totalFolders) {
+		t.Fatal("comparision of changed state with current state does not add up....ouch\n")
+		t.Fail()
+	}
+	if len(deletedPaths) != 2 {
+		t.Fatal(fmt.Sprintf("wrong number of deleted paths. expected 2, got %d....ouch\n", len(deletedPaths)))
+		t.Fail()
+	}
+
+	// get caught up and recreate them all
+	totalFolders -= len(deletedPaths)
+	dirState, err = createListOfFolders(tmpFolder)
+	subDirs = []string{"a", "abd"}
+	for i:= range subDirs {
+		path := fmt.Sprintf("%s/%s", tmpFolder, subDirs[i])
+		err := os.Mkdir(path, os.ModeDir + os.ModePerm)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	totalFolders += len(subDirs)
+	changed, updatedState, newPaths, deletedPaths, matchingPaths = checkForChanges(tmpFolder, dirState)
+	if !changed || (len(newPaths) + len(deletedPaths) + len(matchingPaths) != totalFolders) {
+		t.Fatal("comparision of changed state with current state does not add up....ouch\n")
+		t.Fail()
+	}
+	if len(newPaths) != 2 {
+		t.Fatal(fmt.Sprintf("wrong number of new paths. expected 2, got %d....ouch\n", len(deletedPaths)))
+		t.Fail()
+	}
 
 	_ = emptyState
 }
@@ -62,4 +162,16 @@ func assertEqualsTwoDirTreeMap(t *testing.T, first, second DirTreeMap) {
 		t.Fail()
 	}
 	//todo continue to do the rest of the tests
+}
+
+func addSubDirs(t *testing.T, baseDir string, subDirs []string) {
+	path := baseDir
+	for i:= range subDirs {
+		path += fmt.Sprintf("/%s", subDirs[i])
+		//fmt.Println(path)
+		err := os.Mkdir(path, os.ModeDir + os.ModePerm)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
 }
