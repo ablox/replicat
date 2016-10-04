3eb23388a4fc858ba7828813a914f27653c171f1
Jacob Taylor <jacob@replic.at>
2016-10-03 17:23:39 -0700

    adding delete folders to the test suite

--- filesystemTracker_test.go
+++ filesystemTracker_test.go
@@ -37,4 +37,21 @@ func TestDirectoryStorage(t *testing.T) {
 		t.Fatal(fmt.Sprintf("Found: %v\nExpected: %v\n", folderList, folderTemplate))
 	}
 
+
+	err := tracker.DeleteFolder(folderTemplate[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	folderList = tracker.ListFolders()
+	sort.Strings(folderList)
+
+	folderTemplate = folderTemplate[1:]
+
+	if reflect.DeepEqual(folderList, folderTemplate) == false {
+		t.Fatal(fmt.Sprintf("Found: %v\nExpected: %v\n", folderList, folderTemplate))
+	}
+
+
+
 }
