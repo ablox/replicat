66c1894d843717b58bce5ae0e441d3513caae717
Jacob Taylor <jacob@replic.at>
2016-10-09 13:46:58 -0700

    moving unneded code and cleaning up

--- main.go
+++ main.go
@@ -25,40 +25,8 @@ func main() {
 
 	bootstrapAndServe()
 
-
 	for {
 		time.Sleep(time.Millisecond * 500)
 	}
 
-
-	//listOfFileInfo, err := scanDirectoryContents()
-	//if err != nil {
-	//	   log.Fatal(err)
-	//}
-	//
-	//dotCount := 0
-	//sleepSeconds := time.Duration(25 + rand.Intn(10))
-	//fmt.Printf("Full Sync time set to: %d seconds\n", sleepSeconds)
-	//for {
-	//	// Randomize the sync time to decrease oscillation
-	//	time.Sleep(time.Second * sleepSeconds)
-	//	changed, updatedState, newPaths, deletedPaths, matchingPaths := checkForChanges(listOfFileInfo, nil)
-	//	if changed {
-	//		fmt.Println("\nWe have changes, ship it (also updating saved state now that the changes were tracked)")
-	//		fmt.Printf("@Path report: new %d, deleted %d, matching %d, original %d, updated %d\n", len(newPaths), len(deletedPaths), len(matchingPaths), len(listOfFileInfo), len(updatedState))
-	//		fmt.Printf("@New paths: %v\n", newPaths)
-	//		fmt.Printf("@Deleted paths: %v\n", deletedPaths)
-	//		fmt.Println("******************************************************")
-	//		listOfFileInfo = updatedState
-	//
-	//		// Post the changes to the other side.
-	//		//sendFolderTree(listOfFileInfo)
-	//	} else {
-	//		fmt.Print(".")
-	//		dotCount++
-	//		if dotCount%100 == 0 {
-	//			fmt.Println("")
-	//		}
-	//	}
-	//}
 }
--- tracker.go
+++ tracker.go
@@ -398,3 +398,35 @@ func (p FileInfoSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 // Strings sorts a slice of strings in increasing order.
 //func fileInfos(a []os.FileInfo) { Sort(FileInfoSlice(a)) }
+
+// sample code for monitoring the filesystem
+//listOfFileInfo, err := scanDirectoryContents()
+//if err != nil {
+//	   log.Fatal(err)
+//}
+//
+//dotCount := 0
+//sleepSeconds := time.Duration(25 + rand.Intn(10))
+//fmt.Printf("Full Sync time set to: %d seconds\n", sleepSeconds)
+//for {
+//	// Randomize the sync time to decrease oscillation
+//	time.Sleep(time.Second * sleepSeconds)
+//	changed, updatedState, newPaths, deletedPaths, matchingPaths := checkForChanges(listOfFileInfo, nil)
+//	if changed {
+//		fmt.Println("\nWe have changes, ship it (also updating saved state now that the changes were tracked)")
+//		fmt.Printf("@Path report: new %d, deleted %d, matching %d, original %d, updated %d\n", len(newPaths), len(deletedPaths), len(matchingPaths), len(listOfFileInfo), len(updatedState))
+//		fmt.Printf("@New paths: %v\n", newPaths)
+//		fmt.Printf("@Deleted paths: %v\n", deletedPaths)
+//		fmt.Println("******************************************************")
+//		listOfFileInfo = updatedState
+//
+//		// Post the changes to the other side.
+//		//sendFolderTree(listOfFileInfo)
+//	} else {
+//		fmt.Print(".")
+//		dotCount++
+//		if dotCount%100 == 0 {
+//			fmt.Println("")
+//		}
+//	}
+//}
