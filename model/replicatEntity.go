0d229798368a5567d3e2ba692e747cd8cf8a22a0
Jacob Taylor <jacob@usekahuna.com>
2016-10-25 20:54:38 -0700

    adding lint comments{

--- model/replicatEntity.go
+++ model/replicatEntity.go
@@ -1,10 +1,12 @@
 package model
 
+// ReplicatEntityValue - initial take on metadata for stored contents
 type ReplicatEntityValue struct {
 	hash      string
 	timestamp string
 }
 
+// ReplicatEntityMap - Initial take on structure for storing the contents
 type ReplicatEntityMap struct {
 }
 
