906bc488be6cbd512e5733fb0f86c77c3e0b8e44
Jacob Taylor jacob@replic.at <jacob@replic.at>
2017-06-05 22:40:56 -0700

    comments and license

--- trackerutils.go
+++ trackerutils.go
@@ -1,3 +1,18 @@
+// Package replicat is a server for n way synchronization of content (Replication for the cloud).
+// Copyright 2016 Jacob Taylor jacob@replic.at       More Info: http://replic.at
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package main
 
 import "time"
@@ -38,11 +53,13 @@ func WaitForStorage(tracker StorageTracker, folder string, waitingFor bool, help
 	}
 }
 
+// startTest - log the fact that a test is starting and the test's name
 func startTest(name string) {
 	event := Event{Name: "startTest", Path: name, Source: globalSettings.Name}
 	SendEvent(event, "")
 }
 
+// endTest - log the fact that a test is ending and the test's name
 func endTest(name string) {
 	event := Event{Name: "endTest", Path: name, Source: globalSettings.Name}
 	SendEvent(event, "")
